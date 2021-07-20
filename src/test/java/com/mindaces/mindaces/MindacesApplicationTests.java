b59ad68d5926f1727bbdf395a354db4d43499b5b
praisebak <praisebak@naver.com>
2021-07-21 00:25:17 +0900

    mindaces start

---
+++ src/test/java/com/mindaces/mindaces/MindacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindaces.mindaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
