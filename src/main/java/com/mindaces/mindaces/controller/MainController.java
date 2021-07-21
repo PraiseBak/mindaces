bbca1ec1b9bcd1fc0ba22cab4d65e72ff5d988d1
praisebak <praisebak@naver.com>
2021-07-21 22:32:37 +0900

    update

--- src/main/java/com/mindaces/mindaces/controller/MainController.java
+++ src/main/java/com/mindaces/mindaces/controller/MainController.java
@@ -1,14 +1,27 @@
 package com.mindaces.mindaces.controller;
 
+import com.mindaces.mindaces.dto.BoardDto;
+import com.mindaces.mindaces.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import java.util.List;
 
 @Controller
 public class MainController
 {
-    @GetMapping("/")
-    public String main()
+    private BoardService boardService;
+
+    public MainController(BoardService boardService)
     {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/")
+    public String main(Model model)
+    {
+        //List<BoardDto> boardDtoList = boardService.getBoardList();
+        //model.addAttribute("recommandBoard",boardDtoList);
         return "main/main.html";
     }
 
