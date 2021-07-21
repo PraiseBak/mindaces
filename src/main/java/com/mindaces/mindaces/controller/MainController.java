b9864da0fc30f9809e701f65a632e7db29cd539e
praisebak <praisebak@naver.com>
2021-07-22 01:02:07 +0900

    done with db connect test. but Entity have to modify!!

--- src/main/java/com/mindaces/mindaces/controller/MainController.java
+++ src/main/java/com/mindaces/mindaces/controller/MainController.java
@@ -5,6 +5,8 @@ import com.mindaces.mindaces.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 @Controller
@@ -22,7 +24,23 @@ public class MainController
     {
         //List<BoardDto> boardDtoList = boardService.getBoardList();
         //model.addAttribute("recommandBoard",boardDtoList);
-        return "main/main.html";
+        return "main/main";
     }
+
+    @GetMapping("/post")
+    public String post()
+    {
+        return "main/post";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto)
+    {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+
+
+
 
 }
